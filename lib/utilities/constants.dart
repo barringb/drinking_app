640d62d552517d94dd48c5daa8e19a45ff496b63
Brad Barringer <barringb@gmail.com>
2021-07-21 17:04:13 -0700

    added files & API URL constants

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,7 @@
+
+//  https://www.thecocktaildb.com/api.php
+const kApiKey = '1';
+const kCocktailByNameURL = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+const kRandomCocktailURL = 'www.thecocktaildb.com/api/json/v1/1/random.php';
+const kCocktailByIdURL = 'www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007';
+const kCocktailByIngredientURL = 'www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka';
